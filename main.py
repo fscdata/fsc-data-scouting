e68e13c28d16f513ae7f791cca7627124ba06a87
Erica Pfister <ericaspfister@gmail.com>
2026-02-08 21:13:26 +0000

    incorporate .env file for secrets

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import os
+from dotenv import load_dotenv
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+load_dotenv()  # Loads environment variables from .env file
+
+# Accessing the variables
+database_uri = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app = Flask(__name__)
 
 @app.route("/")
 def scout_page():
     print(' > in scout page')
-    instance = os.environ.get("fsc-db-dev-instance", "INSTANCE Not Found")[:5]
-    username = os.environ.get("fsc-db-dev-username", "USERNAME Not Found")[:5]
-    password = os.environ.get("fsc-db-dev-password", "PASSWORD Not Found")[:5]
-    public_ip = os.environ.get("fsc-db-dev-public-ip", "PUBLIC_IP Not Found")[:5]
-    print(instance, "...")
-    print(username, "...")
-    print(password, "...")
-    print(public_ip, "...")
-    return f"Hello, World! Database instance: {instance}, Username: {username}, Password: {password}, Public IP: {public_ip}"
+    return f"Hello, World! Database instance: {database_uri}"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
