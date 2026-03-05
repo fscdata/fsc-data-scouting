627096a0c82f3c584897d7fde13045a85b618096
Erica Pfister <erica.codes@mailnull.com>
2026-03-05 11:58:41 -0500

    Incremental updates

--- cron/calculate_report_data.py
+++ cron/calculate_report_data.py
@@ -43,13 +43,67 @@ def do_some_math():
         for team_number in event_team_list:
             print(f'Calculating data for team {team_number[0]}')
             # do some math here to calculate average climb level and success rate for the team
-            # this is just an example, replace with actual calculations
+            team_climbs = db.session\
+                .query(
+                    MatchTeamData.team_number,
+                    MatchTeamData.auto_climb_try,
+                    MatchTeamData.auto_climbed,
+                    MatchTeamData.endgame_climb_try,
+                    MatchTeamData.endgame_climb_level)\
+                .filter(MatchTeamData.event_id == event_id)\
+                .all()
+            team_climb_stats = {}
+            for match in team_climbs:
+                team_num = match.team_number
+                if team_num in team_climb_stats:
+                    team_climb_stats[team_num]['match_count'] += 1
+                    prev_auto_climb_try = team_climb_stats[team_num]['auto_climb_try']
+                    prev_auto_climbed = team_climb_stats[team_num]['auto_climbed']
+                    prev_endgame_climb_try = team_climb_stats[team_num]['endgame_climb_try']
+                    prev_endgame_climb_level = team_climb_stats[team_num]['endgame_climb_level']
+                    if match.auto_climb_try is not None:
+                        team_climb_stats[team_num]['auto_climb_try'] = prev_auto_climb_try + 1
+                    if match.auto_climbed is not None:
+                        team_climb_stats[team_num]['auto_climbed'] = prev_auto_climbed + 1
+                    if match.endgame_climb_try is not None:
+                        team_climb_stats[team_num]['endgame_climb_try'] = prev_endgame_climb_try + 1
+                    if match.endgame_climb_level is not None:
+                        team_climb_stats[team_num]['endgame_climb_level'] = prev_endgame_climb_level + 1
+                else:
+                    team_climb_stats[team_num] = {
+                        'match_count': 0,
+                        'auto_climb_try': 0,
+                        'auto_climbed': 0,
+                        'endgame_climb_try': 0,
+                        'endgame_climbed': 0
+                    }
+                    team_climb_stats[team_num]['match_count'] = 1
+                    if match.auto_climb_try is not None:
+                        team_climb_stats[team_num]['auto_climb_try'] = prev_auto_climb_try + 1
+                    if match.auto_climbed is not None:
+                        team_climb_stats[team_num]['auto_climbed'] = prev_auto_climbed + 1
+                    if match.endgame_climb_try is not None:
+                        team_climb_stats[team_num]['endgame_climb_try'] = prev_endgame_climb_try + 1
+                    if match.endgame_climb_level is not None:
+                        team_climb_stats[team_num]['endgame_climbed'] = prev_endgame_climb_level + 1
+            for team_num in team_climb_stats:
+                if team_climb_stats[team_num]['auto_climb_try'] > 0:
+                    team_climb_stats[team_num]['auto_climb_success'] = team_climb_stats[team_num]['auto_climbed'] / team_climb_stats[team_num]['auto_climb_try']
+                else:
+                    team_climb_stats[team_num]['auto_climb_success'] = None
+
+                if team_climb_stats[team_num]['endgame_climb_try'] > 0:
+                    team_climb_stats[team_num]['endgame_climb_success'] = team_climb_stats[team_num]['endgame_climbed'] / team_climb_stats[team_num]['endgame_climb_try']
+                else:
+                    team_climb_stats[team_num]['endgame_climb_success'] = None
+
+
             avg_climb_level = db.session.query(db.func.avg(MatchTeamData.endgame_climb_level))\
                 .filter(MatchTeamData.event_id == event_id, MatchTeamData.team_number == team_number[0])\
                 .scalar()
             db.session.query(Calculation)\
                 .filter(Calculation.event_id == event_id, Calculation.team_number == team_number[0])\
-                .update({Calculation.average_climb_level: avg_climb_level})
+                .update({Calculation.event_climb: avg_climb_level})
         db.session.commit()
 
 if __name__ == "__main__":
