1e462b7564adbd9df135091014d98e691892b660
Erica Pfister <erica.codes@mailnull.com>
2026-08-22 16:32:55 -0400

    new big easy event button for front page -- close #9

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, render_template
+from database_model import db, Event
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def home_page():
+    active_event_id = db.session\
+        .query(Event.event_id)\
+        .filter(Event.event_currently_active == 1)\
+        .scalar()
+    
     print(' > Rendering home page')
-    return render_template('home_page.html')
+    return render_template(
+        'home_page.html',
+        active_event_id=active_event_id)
 
 @bp.route("/confirmed")
 def confirmation_page():
